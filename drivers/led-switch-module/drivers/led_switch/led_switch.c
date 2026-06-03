8209fde6aa34876147922139c3710fc71b40af07
Christian Halter <halter.cd@gmail.com>
2026-06-03 18:08:05 -0300

    API functions implemented

--- drivers/led-switch-module/drivers/led_switch/led_switch.c
+++ drivers/led-switch-module/drivers/led_switch/led_switch.c
@@ -15,6 +15,29 @@ struct led_switch_data {
     bool led_state;
 };
 
+static int led_switch_channel_get(const struct device *dev,
+                                 enum sensor_channel chan,
+                                 struct sensor_value *val)
+{
+    const struct led_switch_config *cfg = dev->config;
+    struct led_switch_data *data = dev->data;
+    gpio_pin_set_dt(&cfg->led, 1);
+    data->led_state = true;
+	LOG_INF("LED ON");
+	return 0;
+}
+
+static int nn_driver_sample_fetch(const struct device *dev,
+                                  enum sensor_channel chan)
+{
+    const struct led_switch_config *cfg = dev->config;
+    struct led_switch_data *data = dev->data;
+    gpio_pin_set_dt(&cfg->led, 0);
+    data->led_state = false;
+    LOG_INF("LED OFF");
+	return 0;
+}
+
 static DEVICE_API(sensor,led_switch_api) = {
 	.sample_fetch = led_switch_sample_fetch,
 	.channel_get = led_switch_channel_get,
