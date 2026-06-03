58c125db6beeb8b98443e14f7910fad407e9a9ac
Christian Halter <halter.cd@gmail.com>
2026-06-03 12:52:41 -0300

    Define driver data and config structures

--- drivers/led-switch-module/drivers/led_switch/led_switch.c
+++ drivers/led-switch-module/drivers/led_switch/led_switch.c
@@ -1,10 +1,19 @@
 
 #include <zephyr/drivers/sensor.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 
 #define DT_DRV_COMPAT led_switch
 
 LOG_MODULE_REGISTER(led_switch, LOG_LEVEL_INF);
+
+struct led_switch_config {
+    struct gpio_dt_spec led;
+};
+
+struct led_switch_data {
+    bool led_state;
+};
 
 static DEVICE_API(sensor,led_switch_api) = {
 	.sample_fetch = led_switch_sample_fetch,
