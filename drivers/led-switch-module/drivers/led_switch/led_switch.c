50641e16ec9c9bf59443f4ca807a4253edd9a456
Christian Halter <halter.cd@gmail.com>
2026-06-03 12:49:47 -0300

    Define sensor driver API

--- drivers/led-switch-module/drivers/led_switch/led_switch.c
+++ drivers/led-switch-module/drivers/led_switch/led_switch.c
@@ -1,6 +1,12 @@
 
+#include <zephyr/drivers/sensor.h>
 #include <zephyr/logging/log.h>
 
 #define DT_DRV_COMPAT led_switch
 
 LOG_MODULE_REGISTER(led_switch, LOG_LEVEL_INF);
+
+static DEVICE_API(sensor,led_switch_api) = {
+	.sample_fetch = led_switch_sample_fetch,
+	.channel_get = led_switch_channel_get,
+};
