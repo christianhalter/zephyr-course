c2ecf661c127e665f048093704fe764986ae5116
Christian Halter <halter.cd@gmail.com>
2026-06-10 20:00:05 -0300

    API exposed

--- drivers/led-switch-module/include/led_switch/led_switch.h
+++ drivers/led-switch-module/include/led_switch/led_switch.h
@@ -1,20 +1,26 @@
 #pragma ONCE
 
 #include <zephyr/device.h>
+#include <zephyr/drivers/sensor.h>
 #include <zephyr/toolchain.h>
 
 #ifdef __cplusplus
 extern "C"{
 #endif
 
-static int led_switch_channel_get(const struct device *dev,
+__subsystem struct led_switch_api {
+	int (*channel_get)(const struct device *dev, enum sensor_channel chan, struct sensor_value *val);
+	int (*sample_fetch)(const struct device *dev, enum sensor_channel chan);
+};
+
+int led_switch_channel_get(const struct device *dev,
                                  enum sensor_channel chan,
                                  struct sensor_value *val);
 
-static int led_switch_sample_fetch(const struct device *dev,
+int led_switch_sample_fetch(const struct device *dev,
                                   enum sensor_channel chan);
 
-static int led_switch_init(const struct device *dev);
+int led_switch_init(const struct device *dev);
 
 #ifdef __cplusplus
 }
