78345f5c26c0e91ecfba28a50298b9ecd8ae61a7
Christian Halter <halter.cd@gmail.com>
2026-06-21 20:00:23 -0300

    Changed app to use driver

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,27 +2,55 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
+#include <led_switch/led_switch.h>
+
 #define SLEEP_TIME_MS CONFIG_APP_HEARTBEAT_PERIOD_MS
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED_NODE DT_ALIAS(app_led)
+/* The devicetree node identifier for the "led_switch" alias. */
+#define LED_SWITCH_NODE DT_ALIAS(led_switch)
 
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+static const struct device *const led_switch = DEVICE_DT_GET(LED_SWITCH_NODE);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 int main(void)
 {
     bool led_state = true;
+    struct sensor_value value;
 
-    if (!gpio_is_ready_dt(&led)) return 0;
-
-    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+    if (!device_is_ready(led_switch))
+    {
+        LOG_ERR("LED Switch is not ready");
+        return 0;
+    }
 
     while (1) {
-        if (gpio_pin_toggle_dt(&led) < 0) return 0;
 
-        led_state = !led_state;
+        if (led_state)
+        {
+            if (sensor_sample_fetch(led_switch, SENSOR_CHAN_ALL)<0)
+            {
+                LOG_ERR("Sample fetch - LED OFF - failed");
+                return 0;
+            }
+            else
+            {
+                led_state = false;
+            }
+        }
+        else
+        {
+            if (sensor_channel_get(led_switch, SENSOR_CHAN_ALL, &value))
+            {
+                LOG_ERR("Channel get - LED ON - failed");
+                return 0;
+            }
+            else
+            {
+                led_state = true;
+            }
+        }
+
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
         k_msleep(SLEEP_TIME_MS);
     }
