10ff949645cb52c2de5760e9ccd0be3fc8c3f942
Christian Halter <halter.cd@gmail.com>
2026-06-30 20:12:32 -0300

    Shell functions implemented

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,6 +1,7 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/shell/shell.h>
 
 #include <led_switch/led_switch.h>
 
@@ -12,6 +13,44 @@
 static const struct device *const led_switch = DEVICE_DT_GET(LED_SWITCH_NODE);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+
+static int cmd_info(const struct shell *sh, size_t argc, char **argv)
+{
+    ARG_UNUSED(argc);
+    ARG_UNUSED(argv);
+    if (!device_is_ready(led_switch))
+    {
+        shell_error(sh, "LED Switch is not ready");
+    }
+    else
+    {
+        shell_print(sh, "LED Switch is ready");
+    }
+    return 0;
+}
+
+static int cmd_fetch(const struct shell *sh, size_t argc, char **argv)
+{
+    ARG_UNUSED(argc);
+    ARG_UNUSED(argv);
+    if (sensor_sample_fetch(led_switch)<0)
+    {
+        shell_error(sh, "Sample fetch - LED OFF - failed");
+    }
+    return 0;
+}
+
+static int cmd_read(const struct shell *sh, size_t argc, char **argv)
+{
+    ARG_UNUSED(argc);
+    ARG_UNUSED(argv);
+    struct sensor_value value;
+    if (sensor_channel_get(led_switch, SENSOR_CHAN_ALL, &value)<0)
+    {
+        shell_error(sh, "Channel get - LED ON - failed");
+    }
+    return 0;
+}
 
 int main(void)
 {
@@ -25,7 +64,7 @@ int main(void)
     }
 
     while (1) {
-
+/*
         if (led_state)
         {
             if (sensor_sample_fetch(led_switch)<0)
@@ -52,6 +91,7 @@ int main(void)
         }
 
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
+*/
         k_msleep(SLEEP_TIME_MS);
     }
     return 0;
